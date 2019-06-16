3208b8c311945e94289190ca1fd37003d41df695
karlomurk <45050919+karlomurk@users.noreply.github.com>
2019-06-16 20:48:47 +0200

    Add files via upload

---
+++ kopiranje_datoteke1/kopiranje_datoteke1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace kopiranje_datoteke1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pokreniTipka = new System.Windows.Forms.Button();
+            this.zaustaviTipka = new System.Windows.Forms.Button();
+            this.napredakTraka = new System.Windows.Forms.ProgressBar();
+            this.napredakTekst = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tekst = new System.Windows.Forms.TextBox();
+            this.tekst1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pokreniTipka
+            // 
+            this.pokreniTipka.Location = new System.Drawing.Point(214, 231);
+            this.pokreniTipka.Name = "pokreniTipka";
+            this.pokreniTipka.Size = new System.Drawing.Size(75, 23);
+            this.pokreniTipka.TabIndex = 0;
+            this.pokreniTipka.Text = "b";
+            this.pokreniTipka.UseVisualStyleBackColor = true;
+            this.pokreniTipka.Click += new System.EventHandler(this.pokreniTipka_Click);
+            // 
+            // zaustaviTipka
+            // 
+            this.zaustaviTipka.Location = new System.Drawing.Point(382, 231);
+            this.zaustaviTipka.Name = "zaustaviTipka";
+            this.zaustaviTipka.Size = new System.Drawing.Size(75, 23);
+            this.zaustaviTipka.TabIndex = 1;
+            this.zaustaviTipka.Text = "button2";
+            this.zaustaviTipka.UseVisualStyleBackColor = true;
+            this.zaustaviTipka.Click += new System.EventHandler(this.zaustaviTipka_Click);
+            // 
+            // napredakTraka
+            // 
+            this.napredakTraka.Location = new System.Drawing.Point(214, 166);
+            this.napredakTraka.Name = "napredakTraka";
+            this.napredakTraka.Size = new System.Drawing.Size(490, 30);
+            this.napredakTraka.TabIndex = 2;
+            // 
+            // napredakTekst
+            // 
+            this.napredakTekst.Location = new System.Drawing.Point(214, 203);
+            this.napredakTekst.Name = "napredakTekst";
+            this.napredakTekst.Size = new System.Drawing.Size(100, 22);
+            this.napredakTekst.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tekst
+            // 
+            this.tekst.Location = new System.Drawing.Point(214, 23);
+            this.tekst.Name = "tekst";
+            this.tekst.Size = new System.Drawing.Size(490, 22);
+            this.tekst.TabIndex = 6;
+            // 
+            // tekst1
+            // 
+            this.tekst1.Location = new System.Drawing.Point(214, 72);
+            this.tekst1.Name = "tekst1";
+            this.tekst1.Size = new System.Drawing.Size(490, 22);
+            this.tekst1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tekst1);
+            this.Controls.Add(this.tekst);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.napredakTekst);
+            this.Controls.Add(this.napredakTraka);
+            this.Controls.Add(this.zaustaviTipka);
+            this.Controls.Add(this.pokreniTipka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pokreniTipka;
+        private System.Windows.Forms.Button zaustaviTipka;
+        private System.Windows.Forms.ProgressBar napredakTraka;
+        private System.Windows.Forms.TextBox napredakTekst;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tekst;
+        private System.Windows.Forms.TextBox tekst1;
+    }
+}
+
---
+++ kopiranje_datoteke1/kopiranje_datoteke1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kopiranje_datoteke1
+{
+    public partial class Form1 : Form
+    {
+
+        private BackgroundWorker radnik1 = new BackgroundWorker();
+
+        public object Filestream { get; private set; }
+        Thread t, t2, t3;
+
+        public Form1()
+
+
+        {
+            InitializeComponent();
+
+
+            radnik1.WorkerReportsProgress = true;
+            radnik1.WorkerSupportsCancellation = true;
+
+            radnik1.DoWork += new DoWorkEventHandler(radnik_DoWork);
+            radnik1.ProgressChanged += new ProgressChangedEventHandler(radnik_ProgressChanged);
+            radnik1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(radnik_RunWorkerCompleted);
+        }
+
+        private void pokreniTipka_Click(object sender, EventArgs e)
+        {
+            if (radnik1.IsBusy != true)
+            {
+                // Metodom RunWorkerAsync se pokreće izvođenje operacije u pozadini
+                radnik1.RunWorkerAsync();
+            }
+        }
+
+       
+
+
+        void kopiranjeDatoteke(string source, string des)
+        {
+
+            FileStream fsOut = new FileStream(des, FileMode.Create);
+            FileStream fsIn = new FileStream(source, FileMode.Open);
+
+            byte[] bit = new byte[1048756];
+            int readByte;
+            while ((readByte = fsIn.Read(bit, 0, bit.Length)) > 0)
+            {
+
+                fsOut.Write(bit, 0, readByte);
+                radnik1.ReportProgress((int)(fsIn.Position * 100 / fsIn.Length));
+
+
+            }
+
+            fsIn.Close();
+            fsOut.Close();
+
+        }
+
+
+        /* DoWork
+        /* https://msdn.microsoft.com/en-us/library/system.componentmodel.backgroundworker.dowork.aspx 
+         * -->
+         * Your code in the DoWork event handler should periodically check the CancellationPending 
+         * property value and abort the operation if it is true. When this occurs, you can set the 
+         * Cancel flag of System.ComponentModel.DoWorkEventArgs to true, and the Cancelled flag of 
+         * System.ComponentModel.RunWorkerCompletedEventArgs in your RunWorkerCompleted event handler 
+         * will be set to true.
+         */
+
+        private void posaoZaRadnika()
+        {
+            System.Threading.Thread.Sleep(50);
+
+
+
+        }
+
+
+        private void radnik_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+
+
+            kopiranjeDatoteke(tekst.Text, tekst1.Text);
+        }
+
+        private void radnik_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // this.tbProgress.Text = (e.ProgressPercentage.ToString() + "%");
+
+            napredakTekst.Text = e.ProgressPercentage.ToString();
+            napredakTraka.Value = e.ProgressPercentage;
+        }
+
+        private void radnik_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            if ((e.Cancelled == true))
+            {
+                napredakTekst.Text = "Otkazano!";
+            }
+
+            else if (!(e.Error == null))
+            {
+                napredakTekst.Text = ("Greška: " + e.Error.Message);
+            }
+            else
+            {
+                napredakTekst.Text = "Gotovo!";
+            }
+        }
+
+        private void zaustaviTipka_Click(object sender, EventArgs e)
+        {
+            radnik1.CancelAsync();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                tekst.Text = o.FileName;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog o = new FolderBrowserDialog();
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                tekst1.Text = Path.Combine(o.SelectedPath, Path.GetFileName(tekst.Text));
+
+            }
+        }
+
+        /* https://msdn.microsoft.com/en-us/library/system.componentmodel.backgroundworker.cancelasync.aspx
+         *
+         * CancelAsync submits a request to terminate the pending background operation and sets the CancellationPending 
+         *
+         * property to true. When you call CancelAsync, your worker method has an opportunity to stop its execution and exit. 
+         * The worker code should periodically check the CancellationPending property to see if it has been set to true.
+         */
+    }
+}
